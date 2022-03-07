22c4db89f763a6211ea65a3a68b26eecd5890491
Tony Lee <blackcoder787@gmail.com>
2022-03-06 23:02:45 -0500

    Uber Clone

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+const options = {
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: 'avoid'
+};
+
+module.exports = options;
